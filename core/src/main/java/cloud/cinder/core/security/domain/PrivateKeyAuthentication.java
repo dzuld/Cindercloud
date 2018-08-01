54cc97d42af853a750f7f5da6d7156686b4c518b
Quinten De Swaef <quintendeswaef@gmail.com>
2018-08-02 00:14:25 +0200

    fix problem with keypairs

--- core/src/main/java/cloud/cinder/core/security/domain/PrivateKeyAuthentication.java
+++ core/src/main/java/cloud/cinder/core/security/domain/PrivateKeyAuthentication.java
@@ -3,23 +3,25 @@ package cloud.cinder.core.security.domain;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.web3j.crypto.ECKeyPair;
 
+import java.math.BigInteger;
+
 import static java.util.Collections.emptyList;
 
 public class PrivateKeyAuthentication extends AbstractAuthenticationToken {
 
-    final ECKeyPair keyPair;
+    final BigInteger privateKey;
     final String address;
     final AuthenticationType type = AuthenticationType.CINDERCLOUD;
 
     public PrivateKeyAuthentication(final ECKeyPair keyPair, final String address) {
         super(emptyList());
-        this.keyPair = keyPair;
+        this.privateKey = keyPair.getPrivateKey();
         this.address = address;
     }
 
     @Override
     public Object getCredentials() {
-        return keyPair;
+        return ECKeyPair.create(privateKey);
     }
 
     @Override
